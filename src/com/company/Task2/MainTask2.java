0c1c00ed43b9f35d97c6f14bd4b3987c406f74d2
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2022-02-02 14:19:54 +0200

    Finish my work with this program

--- src/com/company/Task2/MainTask2.java
+++ src/com/company/Task2/MainTask2.java
@@ -6,24 +6,23 @@ import java.io.IOException;
 
 public class MainTask2 {
 
-    public static void main(String[] args) throws IOException {
-        FileReader readFromFile = new FileReader(args[0]);
-        FileWriter writeToFile = new FileWriter(args[1]);
+    public static void main(String[] args) {
 
         try {
+            FileReader readFromFile = new FileReader(args[0]);
+            FileWriter writeToFile = new FileWriter(args[1]);
+
             int i;
             while ((i = readFromFile.read()) != -1) {
                 writeToFile.write(i);
             }
 
-            System.out.println("File reading and writing both done");
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-
-        } finally {
             readFromFile.close();
             writeToFile.close();
-       }
+
+            System.out.println("File reading and writing both done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
