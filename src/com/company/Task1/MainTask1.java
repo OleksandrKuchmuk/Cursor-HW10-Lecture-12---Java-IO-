4a6d1941117387d29e8d900378a91a6697079fe4
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2022-02-02 13:28:01 +0200

    First commit to this progtam, i continue to work

---
+++ src/com/company/Task1/MainTask1.java
@@ -0,0 +1,21 @@
+package com.company.Task1;
+
+import java.io.IOException;
+
+public class MainTask1 {
+
+    public static void main(String[] args) throws IOException  {
+//
+//        FileReader readFromFile = new FileReader(args[0]);
+//        FileWriter writeToFile = new FileWriter(args[1]);
+//
+//            int i;
+//            while ((i = readFromFile.read()) != -1) {
+//                writeToFile.write((char) i);
+//            }
+//            readFromFile.close();
+//            writeToFile.close();
+//
+//        System.out.println("File reading and writing both done");
+    }
+}
